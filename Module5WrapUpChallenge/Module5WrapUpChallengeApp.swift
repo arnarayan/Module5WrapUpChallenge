e2b392e5057fec1870070668aae1d2a1fb023cdc
Anand.Narayan <arnarayan@gmail.com>
2022-06-08 16:09:08 -0600

    Initial Commit

---
+++ Module5WrapUpChallenge/Module5WrapUpChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module5WrapUpChallengeApp.swift
+//  Module5WrapUpChallenge
+//
+//  Created by Anand Narayan on 2022-06-08.
+//
+
+import SwiftUI
+
+@main
+struct Module5WrapUpChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
